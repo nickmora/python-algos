c8b096c52bdabe054dddffdaea98b1b63cd2fb72
Nick Morales <bick.norales@gmail.com>
2019-10-11 11:42:36 -0700

    updated prime

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -1,14 +1,4 @@
 def prime_checker(num):
-    is_prime = True
-    # i = 2
-    # while i < num//2:
-    #     if num % i == 0:
-    #         is_prime = False
-    #     i = i + 1
-    # # print(is_prime)
-    # return is_prime
-    
-# prime_checker(31)
     if num <= 1:
         return False
     if num <= 3:
@@ -18,9 +8,10 @@ def prime_checker(num):
     i = 5
     while i * i <= num:
         if num % i == 0 or num % (i + 2) == 0:
+            print("It's not prime, it's divisible by ", i, " or ", i +2)
             return False
         i = i + 6
-    
+    print("Yup it's prime")
     return True
 
 def get_prime_factors(num):
@@ -36,6 +27,29 @@ def get_prime_factors(num):
 # get_prime_factors(132)
 
 def get_highest_prime_factor(num):
-    print(get_prime_factors(num)[-1])
+    if num % 2 == 0:
+        print(num / 2)
+        return num / 2
+    i = (num // 2) - 1
+    print(i)
+    while i > 1:
+        if num % i == 0:
+            print(i)
+            if prime_checker(i):
+                print(i)
+                return i
+        i = i - 2
+    # print("Number is prime") 
 
-get_highest_prime_factor(600851475143)
+
+# def get_highest_prime_factor(num):
+    # print(get_prime_factors(num)[-1])
+
+# get_highest_prime_factor(600851475143)
+
+# get_highest_prime_factor(799)
+# get_highest_prime_factor(600851475143)
+# get_highest_prime_factor(783)
+get_highest_prime_factor(261)
+# prime_checker(261)
+
